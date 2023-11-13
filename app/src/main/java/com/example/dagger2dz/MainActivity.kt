88f0cdfc3cd35c5ce0c3deaf0757261c94b9df36
Albert <toroxtyne7@gmail.com>
2023-11-14 00:36:31 +0600

    2

--- app/src/main/java/com/example/dagger2dz/MainActivity.kt
+++ app/src/main/java/com/example/dagger2dz/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var myComponent: MyComponent
     lateinit var dogViewModel: DogViewModel
-
+    // ajkdsajlkdjksad;'askd//
     @Inject
     lateinit var dog: Dog
 
